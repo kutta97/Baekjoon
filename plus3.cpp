82ec3e63ecc54f0bafb0039e25a29072bf66a199
kutta97 <yhjin04170@gmail.com>
2021-07-27 12:39:17 +0900

    modify plus3.cpp

--- plus3.cpp
+++ plus3.cpp
@@ -5,27 +5,16 @@ int main(void) {
 	int	t;
 	int	a;
 	int	b;
-	int	*ary;
-	int	i;
 
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
 	cin >> t;
-	ary = new int[t];
-	i = 0;
-	while (i < t)
+	while (t--)
 	{
 		cin >> a >> b;
-		ary[i] = a + b;
-		i++;
-	}
-	i = 0;
-	while (i < t)
-	{
-		cout << ary[i] << "\n";
-		i++;
+		cout << a + b << "\n";
 	}
 	
 	return (0);
