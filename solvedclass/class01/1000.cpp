00518184295cf2c6745ed27be45df20446b6bb6c
Hyejin Yang <hyejinyang@Hyejinui-MacBookPro.local>
2022-03-06 00:45:35 +0900

    solved 1000: A+B

---
+++ solvedclass/class01/1000.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> a >> b;
+    cout << a + b << "\n";
+
+    return 0;
+}
