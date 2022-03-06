b2602bbb9fcd3d4cd60f9ae3ccde03605d030f22
Hyejin Yang <hyejinyang@Hyejinui-MacBookPro.local>
2022-03-06 15:46:13 +0900

    solved 2439: 별 찍기 - 2

---
+++ solvedclass/class01/2439.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (j > N - i - 2) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
